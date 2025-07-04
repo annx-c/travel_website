8d958ca11210f36b746631a675589a21226d6be3
Ana Cañete <ana.canete13@gmail.com>
2025-07-03 22:57:48 -0300

    Add scroll reveal animations and scroll up button

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 const blurHeader = () =>{
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport - Añade una clase si el desplazamiento inferior es mayor que 50 del viewport
-    this.scrollY >= 50 ? header.classList.add('blur-header') 
+    this.scrollY >= 350 ? header.classList.add('blur-header') 
                        : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
@@ -42,8 +42,48 @@ window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class - Cuando el desplazamiento sea mayor a la altura de la ventana gráfica 350, agregue la clase show-scroll a la etiqueta a con la clase scrollup 
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+    //reset: true // Animation repeat
+
+})
+
+sr.reveal(`.home_data, .explore_data, .explore_user, .footer_container`)
+sr.reveal(`.home_card`, { delay: 600, distance: '60px', interval: 100 })
+sr.reveal(`.about_data, .join_image`, { origin: 'right' })
+sr.reveal(`.about_image, .join_data`, { origin: 'left' })
+sr.reveal(`.popular_card`, { interval: 200})
+
